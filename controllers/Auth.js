169a06d88c2ebdfc0e5750207e029fb9c4ae1be5
Robert <osipovitchr@gmail.com>
2022-03-26 10:09:37 -0400

    create Auth routers & controllers

---
+++ controllers/Auth.js
@@ -0,0 +1,52 @@
+const connection = require('../connection.js');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+// const checkJwt = require('./checkJWT.js')
+
+
+const signup = async (req, res) => {
+   
+    const sql = 'INSERT INTO login_credentials (user_email, user_password) VALUES (?,?)'
+    const {email, password } = req.body
+
+    let hash = await argon2.hash(password)
+
+    connection.query(sql, [email, hash], (err, rows) => {
+        if (err) {
+            console.log(err)
+            res.status(404).send('A problem occured' + err.sqlMessage)
+        } else {
+            res.json(rows)
+        }
+    })
+}
+
+
+const login = async (req, res) => {
+    const {email, password } = req.body
+    const sql = 'SELECT * FROM login_credentials WHERE user_email = ?'
+
+
+    connection.query(sql, [email] , async (err, rows) => {
+    
+        if (err) { return res.status(404).send('A problem occured' + err.sqlMessage)}
+
+        if (!rows[0]) {return res.status(400).send('incorrect credentials')}
+        
+        else {
+
+            let hash = rows[0].password
+            let match = await argon2.verify(hash, password)
+            
+            if (!match) { return res.status(400).send('incorrect credentials')}
+            else {
+                let token = jwt.sign({id: rows[0].id}, process.env.SECRET, {expiresIn: process.env.JWT_EXPIRES})
+                res.json({token})
+
+            }
+          }   
+    })
+}
+
+
+module.exports = { signup, login }
---
+++ routers/Auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const AuthController = require('../controllers/Auth.js')
+const router = express.Router();
+
+
+router.post('/signup', AuthController.signup)
+
+router.post('/login', AuthController.login)
+
+
+module.exports = router;
